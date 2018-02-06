a561587b0e61a2e2b78c6e4d0a147b388039c857
Alec Glassford <glassford@stanford.edu>
2018-02-06 10:21:21 -0800

    Highly dubious transcript search

--- client/util.js
+++ client/util.js
@@ -1,3 +1,7 @@
 export const humanPhone = function humanPhoneFunc(phone) {
   return `${phone.substring(2, 5)}-${phone.substring(5, 8)}-${phone.substring(8, 12)}`;
 };
+
+export const humanIndex = function humanIndex(index) {
+  return index + 1;
+};
