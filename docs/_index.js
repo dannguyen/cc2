c64c3051bdb46c8558a258e13373e82c974a535d
Alec Glassford <glassford@stanford.edu>
2018-03-01 01:38:01 -0800

    Slightly more usable demo version of interactive docs

--- docs/_index.js
+++ docs/_index.js
@@ -4,50 +4,84 @@ const deployFilesBase = 'https://raw.githubusercontent.com/alecglassford/cc2/mas
 
 const getDeployFiles = function getDeployFilesFunc() {
   return fetch('deploy-files.json')
-    .then(res => res.json())
-    .then((filenames) => {
+    .then((res) => {
+      if (!res.ok) throw new Error(`Failed to get list of deploy files: ${res.status} ${res.statusText}`);
+      return res.json();
+    }).then((filenames) => {
       const files = filenames.map(file =>
         fetch(`${deployFilesBase}${file}`)
-          .then(res => res.text())
-          .then(data => ({ file, data })));
+          .then((res) => {
+            if (!res.ok) throw new Error(`Failed to get ${deployFilesBase}${file}: ${res.status} ${res.statusText}`);
+            return res.text();
+          }).then(data => ({ file, data })));
       return Promise.all(files);
     });
 };
 
+const postInit = function postInitBaseFunc(nowToken, body) {
+  return {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${nowToken}`,
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    mode: 'cors',
+  };
+};
+
+const getAlias = function getAliasFunc(id, alias, nowToken) {
+  const postBody = { alias };
+  fetch(`https://api.zeit.co/v2/now/deployments/${id}/aliases`, postInit(nowToken, postBody))
+    .then((aliasRes) => {
+      if (aliasRes.status === 400) {
+        setTimeout(getAlias, 10000, id, alias, nowToken);
+        return;
+      }
+      if (!aliasRes.ok) throw new Error('Could not get preferred URl.');
+      alert(`Done! Your instance is available at ${alias} 🎉`);
+    }).catch(() => {
+      alert('Failed to get your preferred URL. Oh well.');
+    });
+};
+
 const deploy = function deployFunc(env) {
+  const nowToken = document.getElementById('now-token').value;
+  if (!nowToken) {
+    alert('Please enter your Now token.');
+    return;
+  }
+  const name = document.getElementById('subdomain').value;
+  if (!name) {
+    alert('Please enter a preferred URL.');
+    return;
+  }
   getDeployFiles()
     .then((files) => {
       const postBody = {
         env,
         public: true,
         forceNew: true,
-        name: document.getElementById('subdomain').value,
+        name,
         deploymentType: 'NPM',
         files,
       };
-      console.log(postBody);
-      const nowToken = document.getElementById('now-token').value;
-      console.log(nowToken);
-      return fetch('https://api.zeit.co/v3/now/deployments', {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${nowToken}`,
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify(postBody),
-        mode: 'cors',
-      });
+      return fetch('https://api.zeit.co/v3/now/deployments', postInit(nowToken, postBody));
     })
-    .then(nowRes => nowRes.json()).then((success) => {
-      console.log('success?');
-      console.log(success);
+    .then((nowRes) => {
+      if (!nowRes.ok) throw new Error(`Deploy failed: ${nowRes.status} ${nowRes.statusText}`);
+      return nowRes.json();
+    }).then((success) => {
+      const { url, deploymentId } = success;
+      alert(`Your instance is available at ${url} 🎉\nWe're still trying to get ${name}.now.sh - keep this page open.`);
+      getAlias(deploymentId, `${name}.now.sh`, nowToken);
     })
     .catch((err) => {
-      console.error(err);
+      alert(err);
     });
 };
 
-const setEnv = function setEnvFunc() {
+const setEnvAndDeploy = function setEnvAndDeployFunc() {
   const env = {
     AIRTABLE_KEY: document.getElementById('airtable-key').value,
     AIRTABLE_BASE: document.getElementById('airtable-base').value,
@@ -55,14 +89,26 @@ const setEnv = function setEnvFunc() {
     TWILIO_AUTH_TOKEN: document.getElementById('twilio-auth-token').value,
     PASSPHRASE: document.getElementById('passphrase').value,
   };
+  const allEnvSet = Object.entries(env).every(([key, val]) => {
+    if (!val) {
+      alert(`Please set ${key}`);
+      return false;
+    }
+    return true;
+  });
+  if (!allEnvSet) return;
 
-  const gooogleCredsFile = document.getElementById('google-creds').files[0];
+  const googleCredsFile = document.getElementById('google-creds').files[0];
+  if (!googleCredsFile) {
+    alert('Please "upload"your Google key file.');
+    return;
+  }
   const reader = new FileReader();
   reader.onload = function doneReadingGoogleCreds() {
     env.GOOGLE_CREDS_STRING = reader.result;
     deploy(env);
   };
-  reader.readAsText(gooogleCredsFile);
+  reader.readAsText(googleCredsFile);
 };
 
-document.getElementById('deploy').addEventListener('click', setEnv);
+document.getElementById('deploy').addEventListener('click', setEnvAndDeploy);
--- docs/index.js
+++ docs/index.js
@@ -7,49 +7,84 @@ var deployFilesBase = 'https://raw.githubusercontent.com/alecglassford/cc2/maste
 
 var getDeployFiles = function getDeployFilesFunc() {
   return fetch('deploy-files.json')
-    .then(function (res) { return res.json(); })
-    .then(function (filenames) {
+    .then(function (res) {
+      if (!res.ok) { throw new Error(("Failed to get list of deploy files: " + (res.status) + " " + (res.statusText))); }
+      return res.json();
+    }).then(function (filenames) {
       var files = filenames.map(function (file) { return fetch(("" + deployFilesBase + file))
-          .then(function (res) { return res.text(); })
-          .then(function (data) { return ({ file: file, data: data }); }); });
+          .then(function (res) {
+            if (!res.ok) { throw new Error(("Failed to get " + deployFilesBase + file + ": " + (res.status) + " " + (res.statusText))); }
+            return res.text();
+          }).then(function (data) { return ({ file: file, data: data }); }); });
       return Promise.all(files);
     });
 };
 
+var postInit = function postInitBaseFunc(nowToken, body) {
+  return {
+    method: 'POST',
+    headers: {
+      Authorization: ("Bearer " + nowToken),
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    mode: 'cors',
+  };
+};
+
+var getAlias = function getAliasFunc(id, alias, nowToken) {
+  var postBody = { alias: alias };
+  fetch(("https://api.zeit.co/v2/now/deployments/" + id + "/aliases"), postInit(nowToken, postBody))
+    .then(function (aliasRes) {
+      if (aliasRes.status === 400) {
+        setTimeout(getAlias, 10000, id, alias, nowToken);
+        return;
+      }
+      if (!aliasRes.ok) { throw new Error('Could not get preferred URl.'); }
+      alert(("Done! Your instance is available at " + alias + " 🎉"));
+    }).catch(function () {
+      alert('Failed to get your preferred URL. Oh well.');
+    });
+};
+
 var deploy = function deployFunc(env) {
+  var nowToken = document.getElementById('now-token').value;
+  if (!nowToken) {
+    alert('Please enter your Now token.');
+    return;
+  }
+  var name = document.getElementById('subdomain').value;
+  if (!name) {
+    alert('Please enter a preferred URL.');
+    return;
+  }
   getDeployFiles()
     .then(function (files) {
       var postBody = {
         env: env,
         public: true,
         forceNew: true,
-        name: document.getElementById('subdomain').value,
+        name: name,
         deploymentType: 'NPM',
         files: files,
       };
-      console.log(postBody);
-      var nowToken = document.getElementById('now-token').value;
-      console.log(nowToken);
-      return fetch('https://api.zeit.co/v3/now/deployments', {
-        method: 'POST',
-        headers: {
-          Authorization: ("Bearer " + nowToken),
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify(postBody),
-        mode: 'cors',
-      });
+      return fetch('https://api.zeit.co/v3/now/deployments', postInit(nowToken, postBody));
     })
-    .then(function (nowRes) { return nowRes.json(); }).then(function (success) {
-      console.log('success?');
-      console.log(success);
+    .then(function (nowRes) {
+      if (!nowRes.ok) { throw new Error(("Deploy failed: " + (nowRes.status) + " " + (nowRes.statusText))); }
+      return nowRes.json();
+    }).then(function (success) {
+      var url = success.url;
+      var deploymentId = success.deploymentId;
+      alert(("Your instance is available at " + url + " 🎉\nWe're still trying to get " + name + ".now.sh - keep this page open."));
+      getAlias(deploymentId, (name + ".now.sh"), nowToken);
     })
     .catch(function (err) {
-      console.error(err);
+      alert(err);
     });
 };
 
-var setEnv = function setEnvFunc() {
+var setEnvAndDeploy = function setEnvAndDeployFunc() {
   var env = {
     AIRTABLE_KEY: document.getElementById('airtable-key').value,
     AIRTABLE_BASE: document.getElementById('airtable-base').value,
@@ -57,16 +92,31 @@ var setEnv = function setEnvFunc() {
     TWILIO_AUTH_TOKEN: document.getElementById('twilio-auth-token').value,
     PASSPHRASE: document.getElementById('passphrase').value,
   };
+  var allEnvSet = Object.entries(env).every(function (ref) {
+    var key = ref[0];
+    var val = ref[1];
 
-  var gooogleCredsFile = document.getElementById('google-creds').files[0];
+    if (!val) {
+      alert(("Please set " + key));
+      return false;
+    }
+    return true;
+  });
+  if (!allEnvSet) { return; }
+
+  var googleCredsFile = document.getElementById('google-creds').files[0];
+  if (!googleCredsFile) {
+    alert('Please "upload"your Google key file.');
+    return;
+  }
   var reader = new FileReader();
   reader.onload = function doneReadingGoogleCreds() {
     env.GOOGLE_CREDS_STRING = reader.result;
     deploy(env);
   };
-  reader.readAsText(gooogleCredsFile);
+  reader.readAsText(googleCredsFile);
 };
 
-document.getElementById('deploy').addEventListener('click', setEnv);
+document.getElementById('deploy').addEventListener('click', setEnvAndDeploy);
 
 }());
